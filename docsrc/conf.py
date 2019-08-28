9b7d8b1060637750b0a653592cb909f9b60ae71f
Braden Mars <BradenM@users.noreply.github.com>
2019-08-28 14:57:10 -0500

    docs: Generate Docs

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -35,9 +35,8 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.coverage',
     'sphinx.ext.napoleon',
-    'sphinx.ext.autosummary'
+    'sphinx.ext.githubpages'
 ]
-autosummary_generate = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
