f07e150bdbedf96ee4f607c610c8900eb49e1680
Braden Mars <bradenmars@bradenmars.me>
2021-02-12 17:35:01 -0600

    feat(compat): add xplatform method for resolving binary executable path

--- pydngconverter/compat.py
+++ pydngconverter/compat.py
@@ -8,9 +8,10 @@ Handles platform-specific operations.
 import platform
 from enum import Enum, auto
 from pathlib import Path
-from typing import Union
+from typing import Union, List, Tuple
 import os
 import subprocess as subp
+from pydngconverter import utils
 from copy import deepcopy
 
 
@@ -86,3 +87,55 @@ def get_compat_path(path: Union[str, Path]) -> str:
         return wine_path(_path)
 
     return str(_path)
+
+
+def resolve_executable(name_variants: List[str]) -> Tuple[Path, str]:
+    """Resolve platform-specific path to given executable.
+
+    Args:
+        name_variants: List of executable names to look for.
+
+    Returns:
+        Path to executable and platform-specific command to execute.
+    """
+    plat = Platform.get()
+    app_map = {
+        Platform.LINUX: Path("/usr/bin"),
+        Platform.WINDOWS: Path(r"C:\Program Files"),
+        Platform.DARWIN: Path("/Applications"),
+    }
+    app_ext = {
+        Platform.LINUX: "{}",
+        Platform.WINDOWS: "{}.exe",
+        Platform.DARWIN: "{}.app/Contents/MacOS/{}",
+    }
+    app_exec = {
+        Platform.LINUX: "{}",
+        Platform.WINDOWS: "{}",
+        Platform.DARWIN: 'open -a "{}" --args',
+    }
+
+    def _resolve(names: List[str]) -> Path:
+        for name in names:
+            _app_root = app_map.get(plat, app_map[Platform.LINUX])
+            _app_ext = app_ext.get(plat, app_map[Platform.LINUX]).format(name)
+            _app_path = _app_root / _app_ext
+            if _app_path.exists():
+                yield _app_path
+            try:
+                bin_path = utils.locate_program(name)
+            except Exception:
+                pass
+            else:
+                if bin_path is not None:
+                    yield bin_path
+
+    try:
+        exec_path = next(_resolve(name_variants))
+    except StopIteration as e:
+        raise RuntimeError(
+            "Could not locate DNG Converter binary! You can manually provide a path with the PYDNG_CONVERTER_PATH env "
+            "variable. "
+        ) from e
+    else:
+        return exec_path, app_exec.get(plat, app_ext[Platform.LINUX]).format(str(exec_path))
