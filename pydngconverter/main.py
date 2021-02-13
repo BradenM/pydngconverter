ad0cd5914ffa196ee86c76cfe92c8c351521c8dd
Braden Mars <bradenmars@bradenmars.me>
2021-02-13 17:09:13 -0600

    feat(pydng): rewrite DNGConverter to be async and execute conversions in parallel

--- pydngconverter/main.py
+++ pydngconverter/main.py
@@ -6,70 +6,28 @@ pydngconverter.main
 PyDNGConverter Code Module
 """
 
-import subprocess as subproc
-from enum import Enum
+import asyncio
+import logging
+from os import PathLike
 from pathlib import Path
+from typing import Optional, Union
 
 import psutil
-import ray
+from rich.logging import RichHandler
 from wand.image import Image
 
-from pydngconverter import utils
+from pydngconverter import utils, compat, dngconverter
+from pydngconverter.dngconverter import DNGParameters
+from pydngconverter.flags import JPEGPreview
 
-
-class CliFlag(Enum):
-
-    @property
-    def name(self):
-        return ""
-
-    @property
-    def flag(self):
-        return f"-{self.name}{self.value}"
-
-    @classmethod
-    def latest(cls):
-        return list(cls)[-1]
-
-
-class JPEGPreview(CliFlag):
-    NONE = 0
-    MEDIUM = 1
-    FULL = 2
-    EXTRACT = 3
-
-    @property
-    def name(self):
-        return "p"
-
-
-class CRawCompat(CliFlag):
-    TWO_FOUR = 2.4
-    FOUR_ONE = 4.1
-    FOUR_SIX = 4.6
-    FIVE_FOUR = 5.4
-    SIX_SIX = 6.6
-    SEVEN_ONE = 7.1
-    ELEVEN_TWO = 11.2
-
-    @property
-    def name(self):
-        return "cr"
-
-
-class DNGVersion(CliFlag):
-    ONE_ONE = 1.1
-    ONE_THREE = 1.3
-    ONE_FOUR = 1.4
-
-    @property
-    def name(self):
-        return "dng"
-
-
-class Compression(CliFlag):
-    NO = "u"
-    YES = "c"
+logging.basicConfig(
+    level=logging.INFO,
+    format="[bold bright_white]%(name)s:[/][bright_black] %(message)s[/]",
+    datefmt="[%X]",
+    handlers=[RichHandler(markup=True)],
+)
+logger = logging.getLogger("pydngconverter")
+logging.getLogger("asyncio").setLevel(logging.WARNING)
 
 
 class DNGConverter:
@@ -79,25 +37,12 @@ class DNGConverter:
     enums and such to make it easier to use.
 
     Args:
-        source (os.Pathlike): Path to source directory containing raw files
-        dest (os.Pathlike, optional): Path to destination.
+        source: Path to source directory containing raw files
+
+    Keyword Args:
+        dest: Path to destination.
             Defaults to source directory.
-        compressed (Compression, optional): Enable DNG Compression.
-            Defaults to Compression.YES.
-        camera_raw (CRawCompat, optional): Set Camera Raw Compat vers.
-             Defaults to CRawCompat.latest().
-        dng_version (DNGVersion, optional): Set DNG Backwards Compat vers.
-             Defaults to DNGVersion.latest().
-        jpeg_preview (JPEGPreview, optional): JPEG Preview size.
-             Defaults to JPEGPreview.MEDIUM.
-        fast_load (bool, optional): Embed fast load data (for jpeg preview).
-             Defaults to False.
-        linear (bool, optional): Output linear DNG files.
-             Defaults to False.
-        multiprocess (bool, optional): Convert multiple files at once.
-             Defaults to True.
-        ray_args (dict, optional): Additonal args to pass to ray.
-             Defaults to {}.
+        **params: DNG Converter parameters.
 
     Raises:
         FileNotFoundError: DNG Converter cannot be found.
@@ -105,198 +50,151 @@ class DNGConverter:
              or is not a directory.
     """
 
-    def __init__(self,
-                 source,
-                 dest=None,
-                 compressed=Compression.YES,
-                 camera_raw=CRawCompat.latest(),
-                 dng_version=DNGVersion.latest(),
-                 jpeg_preview=JPEGPreview.MEDIUM,
-                 fast_load=False,
-                 linear=False,
-                 multiprocess=True,
-                 ray_args={}):
-        self.prog_path = self.resolve_binary("dngconverter")
-        self.compressed = compressed.flag
-        self.camera_raw = camera_raw.flag
-        self.dng_version = dng_version.flag
-        self.jpeg_preview = jpeg_preview
-        self.dest_path = dest
-        self.linear = self.resolve_flag("l", linear)
-        self.fast_load = self.resolve_flag("fl", fast_load)
-        self.dest_path = self.resolve_flag(dest, dest, Path)
-        self.multiprocess = multiprocess
-
-        if self.multiprocess and not ray.is_initialized():
-            self.cpu_count = psutil.cpu_count(logical=False)
-            ray.init(**ray_args)
-
-        if self.jpeg_preview == JPEGPreview.EXTRACT:
-            self.exif_path = self.resolve_binary("exiftool")
-
-        self.source = utils.ensure_existing_dir(source)
+    def __init__(
+        self, source: Union[str, Path], dest: Optional[PathLike] = None, debug=False, **params: DNGParameters
+    ):
+        if debug:
+            logger.setLevel(logging.DEBUG)
+            # enable benchmarking.
+            self.convert = utils.timeit(self.convert)
+        self.parameters = DNGParameters(**params)
+        self.bin_path, self.bin_exec = compat.resolve_executable(
+            ["Adobe DNG Converter", "dngconverter"], "PYDNG_DNG_CONVERTER"
+        )
+        if self.parameters.jpeg_preview == JPEGPreview.EXTRACT:
+            self.exif_path, self.exif_exec = compat.resolve_executable(["exiftool"], "PYDNG_EXIF_TOOL")
+        self.source: Path = Path(source)
+        self.source = utils.ensure_existing_dir(self.source)
         if not self.source:
-            raise NotADirectoryError(
-                f"{source} does not exists or is not a directory!")
+            raise NotADirectoryError(f"{source} does not exists or is not a directory!")
         self.source = self.source.absolute()
-
-    def resolve_binary(self, binary_name):
-        """Resolves path to binary executable
-
-        Args:
-            binary_name (str): Executable name
-
-        Raises:
-            FileNotFoundError: Raised when binary is not found
-
-        Returns:
-            os.Pathlike: Path to binary
-        """
-        path = utils.locate_program(binary_name)
-        if not path:
-            raise FileNotFoundError(f"{binary_name} is not installed!")
-        return path
-
-    def resolve_flag(self, flag, value, on_true=None):
-        """Resolves boolean or special flags
-
-        Args:
-            flag (str): Flag that DNGConverter takes.
-            value (any): Value used to resolve.
-            on_true (callable, optional): Alternative callback on true.
-                 Defaults to None.
-
-        Returns:
-            str: Resolved flag as string.
-        """
-        if value:
-            if on_true:
-                return on_true(flag)
-            return f"-{flag}"
-        return ""
+        self.job = dngconverter.DNGBatchJob(source_directory=self.source, dest_directory=Path(dest))
+        self.chunk_size = psutil.cpu_count()
+        self._queue = asyncio.Queue()
 
     @property
-    def dest(self):
-        """Destination flag property
+    def will_extract(self) -> bool:
+        """Whether to create thumbnail extraction jobs or not."""
+        return self.parameters.jpeg_preview == JPEGPreview.EXTRACT
 
-        Returns:
-            tuple: Tuple containing flag and destination
-        """
-        if not self.dest_path:
-            return ("", "")
-        return ("-d", str(self.dest_path.absolute()))
-
-    @property
-    def args(self):
-        """Args that will be passed to DNGConverter
-
-        Returns:
-            list: List of args to pass
-        """
-        _args = [str(self.prog_path), self.compressed,
-                 self.camera_raw, self.dng_version,
-                 self.fast_load, self.linear]
-        if self.jpeg_preview != JPEGPreview.EXTRACT:
-            _args.append(self.jpeg_preview.flag)
-        return _args
-
-    def convert_multiproc(self, raw_files):
-        """Convert with multiprocessing.
+    async def _write_thumbnail(
+        self, *, job: dngconverter.DNGJob = None, image_bytes=None, log=None, **kwargs
+    ):
+        """Writes thumbnail from bytes extracted from raw image.
 
         Args:
-            raw_files ([os.Pathlike]): Paths to raw files
+            job: current conversion job.
+            image_bytes: image blob data.
+            log: logger to use.
+
+        """
+        log = log or logger
+        log.debug("starting write thumbnail: %s", job.thumbnail_filename)
+
+        @utils.force_async
+        def _write(image_blob, img_dest):
+            with Image(blob=image_blob) as img:
+                img.resize(int(img.width * 0.10), int(img.height * 0.10))
+                img.save(filename=str(img_dest))
+
+        await _write(image_bytes, job.thumbnail_destination)
+        log.info("[bold cyan]wrote thumbnail:[/][bold white] %s[/]", job.thumbnail_filename)
+
+    async def extract_thumbnail(
+        self, *, job: dngconverter.DNGJob = None, log=None, **kwargs
+    ) -> Path:
+        """Extract jpeg thumbnail from exif data.
+
+        Args:
+            job: DNG Converter job.
+            log: Logger to use.
 
         Returns:
-            tuple: List of DNG paths,
-                List of thumbnail paths if JPEGPreview.EXTRACT is passed.
-                Defaults to None
+            Path to thumbnail.
+
         """
-        rem_extract_thumb = ray.remote(DNGConverter.extract_thumbnail)
-        rem_convert_path = ray.remote(DNGConverter.convert_file)
-        raw_files = [str(p) for p in raw_files]
-        thumb_ids = []
-        if self.jpeg_preview == JPEGPreview.EXTRACT:
-            thumb_ids = [rem_extract_thumb.remote(
-                i, self.dest, self.exif_path) for i in raw_files]
-        img_ids = [rem_convert_path.remote(
-            i, self.dest, self.args) for i in raw_files]
-        total_tasks = len(thumb_ids) + len(img_ids)
-        all_tasks = img_ids + thumb_ids
-        completed = set()
+        log = log or logger
+
+        log.info("[bold white]extracting raw thumbnail:[/] [bold grey58]%s => %s[/]", job.source.name, job.thumbnail_filename)
+        exif_args = ["-b", "-previewImage", str(job.source)]
+        exif_proc = await asyncio.create_subprocess_exec(
+            self.exif_exec,
+            *exif_args,
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,
+        )
+        stdout, stderr = await exif_proc.communicate()
+        self._queue.put_nowait(
+            (
+                job,
+                self._write_thumbnail,
+                dict(image_bytes=stdout),
+            )
+        )
+        log.info("[bright_black]queued thumbnail: %s[/]", job.thumbnail_filename)
+        return job.thumbnail_destination
+
+    async def convert_file(
+        self, *, destination: str = None, job: dngconverter.DNGJob = None, log=None
+    ):
+        """Execute provided conversion job.
+
+        Args:
+            destination: Output path.
+            job: DNG Converter job to run.
+            log: Logger to use.
+
+        """
+        log = log or logger
+        log.debug("starting conversion: [b white]%s[/]", job.source.name)
+        source_path = await compat.get_compat_path(job.source)
+        log.debug("determined source path: [b white]%s[/]", source_path)
+        dng_args = [*self.parameters.iter_args, "-d", destination, str(source_path)]
+        log.debug("using converter args: %s", ",".join(dng_args))
+        log.info("[b white]converting:[/] [bold grey58]%s => %s[/]", job.source.name, job.destination_filename)
+        proc = await asyncio.create_subprocess_exec(
+            self.bin_exec, *dng_args, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+        )
+        await proc.wait()
+        log.info("[bold bright_green]finished conversion:[/][bold white] %s[/]", job.destination_filename)
+
+    async def create_worker(self, name: str):
+        """Create job execution worker.
+
+        Args:
+            name: friendly name for worker.
+
+        """
+        worker_log = logger.getChild(name)
         while True:
-            return_count = ((self.cpu_count//2) + len(completed))
-            if return_count > total_tasks:
-                diff = (return_count - total_tasks)
-                return_count = (return_count - diff)
-            ready_ids, remaining_ids = ray.wait(
-                all_tasks, num_returns=return_count)
-            for _i in ready_ids:
-                if _i not in completed:
-                    yield ray.get(_i)
-                completed.add(_i)
-            if len(remaining_ids) == 0:
-                break
+            worker_log.debug("worker awaiting job (qsize: %s)...", self._queue.qsize())
+            item = await self._queue.get()
+            job, action, kwargs = item
+            worker_log.debug("received job: %s (%s)", job, action)
+            await action(job=job, **kwargs, log=worker_log)
+            worker_log.debug("worker finished job!")
+            self._queue.task_done()
 
-    def convert(self):
-        """Convert all files in source directory"""
-        files = self.source.rglob("*.CR2")
-        _, dest_path = self.dest
-        Path(dest_path).mkdir(exist_ok=True)
-        if self.multiprocess:
-            return self.convert_multiproc(files)
-        thumbs = None
-        if self.jpeg_preview == JPEGPreview.EXTRACT:
-            thumbs = [self.extract_thumbnail(
-                p, self.dest, self.exif_path) for p in files]
-        converted = [self.convert_file(
-            str(p), self.dest, self.args) for p in files]
-        return (converted, thumbs)
+    async def convert(self):
+        """Recursively convert all files in source directory."""
+        destination = await compat.get_compat_path(self.job.jobs[0].destination.parent)
+        for job in self.job.jobs:
+            logger.debug("queueing job: %s", job.source.name)
+            self._queue.put_nowait((job, self.convert_file, dict(destination=destination)))
+            if self.will_extract:
+                self._queue.put_nowait((job, self.extract_thumbnail, dict()))
 
-    @staticmethod
-    def extract_thumbnail(path, dest, exif_path=None):
-        """Extract thumbnail from exif data
+        tasks = []
+        num_workers = self.chunk_size
+        logger.debug("creating %s workers!", num_workers)
 
-        Args:
-            path (str): path to raw file
-            dest (str): path to save thumbnail
-            exif_path (str): Path to exiftool
+        for i in range(num_workers + 1):
+            task = asyncio.create_task(self.create_worker(f"worker{i}"))
+            tasks.append(task)
 
-        Returns:
-            str: thumbnail path
-        """
-        if not path:
-            return
-        exif_path = exif_path or "exiftool"
-        args = f"{exif_path} -b -previewImage".split()
-        thumb_bytes = subproc.run([*args, path], stdout=subproc.PIPE).stdout
-        out_name = Path(path).with_suffix('.thumb.jpg').name
-        out_path = Path(dest[1]) / out_name
-        print(f"Extracting Thumbnail: {out_name}")
-        with Image(blob=thumb_bytes) as img:
-            img.resize(int(img.width*.10), int(img.height*.10))
-            img.rotate(270)
-            img.save(filename=str(out_path))
-        return str(out_path)
+        await self._queue.join()
+        logger.debug("queue(s) empty! canceling workers...")
+        for task in tasks:
+            task.cancel()
 
-    @staticmethod
-    def convert_file(path, dest, dng_args):
-        """Converts a single file to .DNG"
-
-        Args:
-            path (os.Pathlike): Path to file.
-            dest (tuple): Destination flag and path.
-            dng_args ([str]): Additional args to pass to DNG Converter.
-
-        Returns:
-           str: Path to DNG file
-        """
-        if not path:
-            return
-        suffix = path.split('.')[-1]
-        dest_file = path.replace(f".{suffix}", ".dng")
-        d_flag, d_parent = dest
-        dng_args.extend([*dest, str(path)])
-        print(f"Converting: {path} => {dest_file}")
-        subproc.run(dng_args, stdout=subproc.DEVNULL,
-                    stderr=subproc.DEVNULL)
-        return dest_file
+        await asyncio.gather(*tasks, return_exceptions=True)
