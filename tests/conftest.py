8baf946e784bffa2f33f167819f8ad819f842c37
Braden Mars <bradenmars@bradenmars.me>
2021-02-14 01:08:39 -0600

    test(mock): add session automock for magic module

---
+++ tests/conftest.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""PyDNGConverter test config."""
+
+import pytest
+from pytest_mock import MockFixture
+
+
+@pytest.fixture(autouse=True, scope="session")
+def mock_wand(session_mocker: MockFixture):
+    session_mocker.patch('pydngconverter.main.Image')
