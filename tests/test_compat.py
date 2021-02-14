b1fac24987892a4307fa4d674cfb0311c2ae92a2
Braden Mars <bradenmars@bradenmars.me>
2021-02-14 00:45:28 -0600

    test(compat): add resolve executable tests

--- tests/test_compat.py
+++ tests/test_compat.py
@@ -4,6 +4,8 @@
 
 import pytest
 from pytest_mock import MockFixture
+import sys
+import os
 
 from pydngconverter import compat
 
@@ -23,6 +25,16 @@ def platform_paths(mock_platform):
     }
     return mock_platform, *map[mock_platform]
 
+@pytest.fixture
+def platform_apps(mock_platform):
+    map = {
+        'Linux': '/usr/bin/ExampleApp',
+        'Windows': r'C:\Program Files\ExampleApp.exe',
+        'Darwin': '/Applications/ExampleApp.app/Contents/MacOS/ExampleApp',
+
+    }
+    return mock_platform, map[mock_platform]
+
 
 @pytest.mark.asyncio
 async def test_get_compat_path(mocker: MockFixture, platform_paths):
@@ -36,3 +48,27 @@ async def test_get_compat_path(mocker: MockFixture, platform_paths):
         mock_proc.assert_called_once()
     else:
         mock_proc.assert_not_called()
+
+
+def test_resolve_executable(mocker: MockFixture, platform_apps):
+    platform, expect_path = platform_apps
+    mocker.patch('pydngconverter.compat.Path.exists', return_value=True)
+    if platform == 'Windows' and not sys.platform.startswith('win'):
+        pytest.skip('skipping windows-only test')
+    ret_path = compat.resolve_executable(['ExampleApp'])
+    assert str(ret_path) == str(expect_path)
+
+
+def test_resolve_executable_override(mocker):
+    mocker.patch('pydngconverter.compat.Path.exists', return_value=True)
+    os.environ['PYDNG_TEST_KEY'] = '/some/fake/path_exec'
+    assert str(compat.resolve_executable(['path_exec'], env_override='PYDNG_TEST_KEY')) == '/some/fake/path_exec'
+
+
+def test_platform_enum(mock_platform):
+    if mock_platform == 'Windows':
+        assert compat.Platform.get().is_win
+    if mock_platform == 'Linux':
+        assert compat.Platform.get().is_nix
+    if mock_platform == 'Darwin':
+        assert compat.Platform.get().is_darwin
