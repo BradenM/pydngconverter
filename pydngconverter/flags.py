21553b4544a5fad28dde220584abc1af105d1a62
Braden Mars <bradenmars@bradenmars.me>
2021-02-14 01:37:50 -0600

    docs(flags): add docstrings to flags module

--- pydngconverter/flags.py
+++ pydngconverter/flags.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
 
-"""PyDNGConverter Flag Enums."""
+"""PyDNGConverter Flag Enums.
+
+Provides mappings for Adobe DNG Converter's parameters.
+
+See Also:
+    Command Line Support in the Adobe DNG Converter:
+        https://wwwimages2.adobe.com/content/dam/acom/en/products/photoshop/pdfs/dng_commandline.pdf
+
+"""
 
 from enum import Enum
 
@@ -20,6 +28,15 @@ class CliFlag(Enum):
 
 
 class JPEGPreview(CliFlag):
+    """JPEG Preview/Thumbnail.
+
+    Setting to "Extract" will
+    extract a thumbnail via the raw image's exif data.
+
+    All other options are passed and handled via Adobe DNG Converter.
+
+    """
+
     NONE = 0
     MEDIUM = 1
     FULL = 2
@@ -31,6 +48,8 @@ class JPEGPreview(CliFlag):
 
 
 class CRawCompat(CliFlag):
+    """Camera Raw Compatibility version."""
+
     TWO_FOUR = 2.4
     FOUR_ONE = 4.1
     FOUR_SIX = 4.6
@@ -45,6 +64,8 @@ class CRawCompat(CliFlag):
 
 
 class DNGVersion(CliFlag):
+    """DNG backwards compatibility version"""
+
     ONE_ONE = 1.1
     ONE_THREE = 1.3
     ONE_FOUR = 1.4
@@ -55,11 +76,15 @@ class DNGVersion(CliFlag):
 
 
 class Compression(CliFlag):
+    """DNG Lossless Compression."""
+
     NO = "u"
     YES = "c"
 
 
 class LossyCompression(CliFlag):
+    """DNG Lossy Compression."""
+
     NO = ""
     YES = "-lossy"
 
