8719fec042efa48389fe31555651c4ac1f3642d8
Braden Mars <bradenmars@bradenmars.me>
2023-03-04 06:53:20 -0600

    chore(master): release 0.3.0

--- pydngconverter/__init__.py
+++ pydngconverter/__init__.py
@@ -8,7 +8,7 @@ speed up the process of converting RAW images to DNG.
 """
 
 __author__ = """Braden Mars"""
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 from pydngconverter import flags
 from pydngconverter.main import DNGConverter, DNGParameters
